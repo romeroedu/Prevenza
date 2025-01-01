08265243fefeb7d8f7b237f23db8e4f6dd7d409a
Eduardo Romero <eromero54@miners.utep.edu>
2024-12-31 21:06:15 -0700

    Added changes to coloring scheme. Now contains 'sky' bg color.

--- prevenza-web/src/components/Header.jsx
+++ prevenza-web/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 text-white py-4 shadow-md">
+    <header className="bg-sky-900 text-white py-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center px-4">
         <h1 className="text-2xl font-bold">Prevenza</h1>
         <nav>
